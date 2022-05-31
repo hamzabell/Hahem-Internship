88e4d81833d437a88019e92f82cea012d2b84a79
Basseyak <Basseyak@ha-shem.com>
2022-05-31 12:09:18 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles/main.scss";
+import "bootstrap";
+
+import AppLogo from "./assets/Logo_2.png";
+import trophyImg from "./assets/trophy.png";
+
+const logo = document.getElementById("logo");
+logo.src = AppLogo;
+
+const trophy = document.getElementById("trophy");
+trophy.src = trophyImg;
